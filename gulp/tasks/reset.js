d182c98375feb43018435122b083e18cae5d4c21
James Sheen <james@Jamess-MacBook-Pro.local>
2017-03-15 11:47:41 +0000

    Added gulp-reset task to revert repo back to original state

--- gulp/tasks/reset.js
+++ gulp/tasks/reset.js
@@ -1,4 +1,3 @@
-
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var gulpif = require('gulp-if');
@@ -6,35 +5,26 @@ var config = require('../config');
 
 
 
-var inquirer = require('inquirer');
 
+// Remove directories defined in DEST below
 
+ var DEST = [
+        'node_modules',
+        'bower_components',
+        'min',
+        '.sass-cache',
+        'src/libs'
+    ]
 
-gulp.task('reset', function(done) {
-    inquirer.prompt([{
-        type: 'confirm',
-        message: 'Do you really want to reset?',
-        default: true,
-        name: 'start'
-    }], function(answers) {
-        if(answers.start) {
-            gulp.start('reset-all');
-        }
-        done();
-    });
-});
-
-// Remove assets folder prior to build
-gulp.task('reset-all', function() {
-
-    var DEST = [
-            'node_modules',
-            'bower_components',
-            'min',
-            '.sass-cache',
-            'src/libs'
-        ]
-
+gulp.task('reset', function() {
     return gulp.src(DEST, { read: false })
+        .pipe($.confirm({
+            question: function() {
+                var files = require('glob').sync('**/*');
+                return !files.length ? false : // No file. And do nothing. 
+                    files.length + ' files are going to be removed. Are you sure? Y/n';
+            },
+            input: '_key:y'
+        }))
         .pipe($.clean());
 });
